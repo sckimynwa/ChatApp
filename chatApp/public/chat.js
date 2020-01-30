a81c9832beb6305d49a49b362bad0bdadfb116f1
sckimynwa <tsb03672@naver.com>
2020-01-30 11:38:53 +0900

    client-side added

---
+++ chatApp/public/chat.js
@@ -0,0 +1,41 @@
+$(function () {
+
+  // make connection
+  var socket = io.connect('http://localhost:3000')
+
+  // buttons inputs
+  var message = $("#message")
+  var username = $("#username")
+  var send_message = $("send_message")
+  var send_username = $("send_username")
+  var chatroom = $("chatroom")
+  var feedback = $("feedback")
+
+  // emit mesasge
+  send_message.click(function () {
+    socket.emit('new_message', { message: message.val() })
+  })
+
+  // listen on new message
+  socket.on("new_message", (data) => {
+    feedback.html('');
+    message.val('');
+    chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+  })
+
+  // emit a username
+  send_username.click(function () {
+    socket.emit('change_username', { username: username.val() })
+  })
+
+  // emit typing
+  message.bind("keypress", () => {
+    socket.emit('typing')
+  })
+
+  // listen on typing
+  socket.on('typing', (data) => {
+    feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+  })
+
+})
